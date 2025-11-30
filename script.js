abbfa6039df5a3f30a3dfb78c6dab47fbbc005ba
givanyo <giovanniviana22@gmail.com>
2025-11-30 15:03:38 -0300

    secao seguranca

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ const elementsAppear = gsap.utils.toArray(".appear-animation");
 const overlapLg = gsap.utils.toArray(".overlap-lg");
 const overlapMd = gsap.utils.toArray(".overlap-md");
 
+gsap.from(".main-text", {
+    y: 100,
+    opacity: 0,
+    duration: 2,
+})
+gsap.from(".card-lg-animated", {
+    width: "80%",
+    scrollTrigger: {
+        trigger: ".card-lg-animated",
+        scrub: 1,
+        start: "top 100%",
+        end: "center 100%",
+    }
+})
+
+gsap.from(".img-overlap", {
+    y: 150,
+    scrollTrigger: {
+        trigger: ".img-overlap",
+        scrub: 1,
+        start: "center 100%",
+        end: "bottom 100%",
+    }
+})
 elementsAppear.forEach(el => {
     gsap.from(el, {
     y: 100,
@@ -18,32 +42,29 @@ elementsAppear.forEach(el => {
 })
 })
 
-gsap.from(".main-text", {
-    y: 100,
-    opacity: 0,
-    duration: 2,
-})
+
 
 overlapLg.forEach(el => {
-    gsap.to(el, {
-        y: 200,
+    gsap.from(el, {
+        y: 350,
         scrollTrigger: {
             trigger: el,
             scrub: 2,
-            start: "top center",
-            end: "bottom center"
+            start: "top 140%",
+            end: "center 100%",
+            markers: true
         }
     })
 })
 
 overlapMd.forEach(el => {
-    gsap.to(el, {
-        y: 220,
+    gsap.from(el, {
+        y: 370,
         scrollTrigger: {
             trigger: el,
             scrub: 2,
-            start: "top center",
-            end: "bottom 60%"
+            start: "top 140%",
+            end: "center 100%"
         }
     })
 })
